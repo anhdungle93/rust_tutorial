c0bfe4bb3f64a9a6769aebe4b07d92ac4bfe94b9
anhdungle <anhdungle634@gmail.com>
2020-06-05 22:51:38 +0200

    program branches demonstrating if statement

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+/Users/anhdungle/learning/rust_tutorial/branches/target/debug/branches: /Users/anhdungle/learning/rust_tutorial/branches/src/main.rs
---
+++ branches/target/debug/deps/branches-3dafc54b52d2eef6.d
@@ -0,0 +1,5 @@
+/Users/anhdungle/learning/rust_tutorial/branches/target/debug/deps/branches-3dafc54b52d2eef6: src/main.rs
+
+/Users/anhdungle/learning/rust_tutorial/branches/target/debug/deps/branches-3dafc54b52d2eef6.d: src/main.rs
+
+src/main.rs:
