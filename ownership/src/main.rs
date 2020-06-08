6f2bc0d572ee2e8ccc4f6023236dc26668b584e7
anhdungle <anhdungle634@gmail.com>
2020-06-08 21:39:35 +0200

    ownership update

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     let s1 = String::from("hello");
     let s2 = s1;
-
-    println!("{}, world!", s1);
+    // the following wouldn't work because the reference s1 was dropped
+    /* println!("{}, world!", s1); */
 }
