304c78470bf4943286f9731922cd8dc08288fed1
anhdungle <anhdungle634@gmail.com>
2020-06-08 21:36:10 +0200

    program to demonstrate structs

---
+++ rectangle/target/debug/deps/rectangle-ca0045fe7d096052.d
@@ -0,0 +1,5 @@
+/Users/anhdungle/learning/rust_tutorial/rectangle/target/debug/deps/rectangle-ca0045fe7d096052: src/main.rs
+
+/Users/anhdungle/learning/rust_tutorial/rectangle/target/debug/deps/rectangle-ca0045fe7d096052.d: src/main.rs
+
+src/main.rs:
---
+++ rectangle/target/debug/rectangle.d
@@ -0,0 +1,1 @@
+/Users/anhdungle/learning/rust_tutorial/rectangle/target/debug/rectangle: /Users/anhdungle/learning/rust_tutorial/rectangle/src/main.rs
